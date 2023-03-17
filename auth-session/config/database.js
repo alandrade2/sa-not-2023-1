be3d2c88296c268c9f567abcaec9e8f6676451e9
ANDRE ANDRADE <67979217+alandrade2@users.noreply.github.com>
2023-03-17 23:27:10 +0000

    cross-site request forgery 17/03

---
+++ auth-session/config/database.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg')
+
+
+const conn = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
+
+// Testando a conexao
+async function testConn() {
+    try {
+        await conn.query('select now()')
+        console.log('** POSTGRES: Conexao estabelecida')
+    } catch (error) {
+        console.error('** POSTGRES> ERRO => ' + error)
+    }
+}
+
+testConn()
+
+module.exports = conn
